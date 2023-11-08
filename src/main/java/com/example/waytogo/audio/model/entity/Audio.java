e5ef2ad2b68d1c633e468b8905692e46b9d74db0
Kondziow <konradwojtanowski02@gmail.com>
2023-11-08 11:19:05 +0100

    User Entity mapping without errors

--- src/main/java/com/example/waytogo/audio/model/entity/Audio.java
+++ src/main/java/com/example/waytogo/audio/model/entity/Audio.java
@@ -1,4 +1,18 @@
 package com.example.waytogo.audio.model.entity;
 
+
+import com.example.waytogo.user.model.entity.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+import java.util.UUID;
+
+@Entity
 public class Audio {
+    @Id
+    UUID uuid;
+
+    @ManyToOne
+    User user;
 }
