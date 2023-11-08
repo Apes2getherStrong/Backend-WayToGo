6914b134b968e2b9a3f0184abfc03f3f3f523f09
Kondziow <konradwojtanowski02@gmail.com>
2023-11-08 11:19:05 +0100

    User Entity mapping without errors

--- src/main/java/com/example/waytogo/route/model/entity/Route.java
+++ src/main/java/com/example/waytogo/route/model/entity/Route.java
@@ -1,4 +1,17 @@
 package com.example.waytogo.route.model.entity;
 
+import com.example.waytogo.user.model.entity.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+import java.util.UUID;
+
+@Entity
 public class Route {
+    @Id
+    UUID uuid;
+
+    @ManyToOne
+    User user;
 }
