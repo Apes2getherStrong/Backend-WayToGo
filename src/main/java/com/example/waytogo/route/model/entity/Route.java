5d2bbc67d6c92ce8239616224763ddb2d5a8b3fd
Drillllll <118617644+Drillllll@users.noreply.github.com>
2023-11-10 15:36:12 +0100

    route added

--- src/main/java/com/example/waytogo/route/model/entity/Route.java
+++ src/main/java/com/example/waytogo/route/model/entity/Route.java
@@ -4,14 +4,23 @@ import com.example.waytogo.user.model.entity.User;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.*;
 
 import java.util.UUID;
 
+@Table(name="route")
+@EqualsAndHashCode
+@Builder
+@Getter
+@Setter
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Route {
     @Id
-    UUID uuid;
-
+    private UUID id;
     @ManyToOne
-    User user;
+    private User user;
+    private String name;
 }
