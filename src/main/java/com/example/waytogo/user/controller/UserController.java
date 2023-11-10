d64bb97c10e7a2e2014f41da7a35407370e81278
Kondziow <konradwojtanowski02@gmail.com>
2023-11-10 14:11:12 +0100

    UserController.getUserById

--- src/main/java/com/example/waytogo/user/controller/UserController.java
+++ src/main/java/com/example/waytogo/user/controller/UserController.java
@@ -3,10 +3,17 @@ package com.example.waytogo.user.controller;
 import com.example.waytogo.user.model.dto.UserDTO;
 import com.example.waytogo.user.service.api.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.UUID;
 
 @RequiredArgsConstructor
 @RestController
@@ -18,7 +25,13 @@ public class UserController {
 
     @GetMapping(USER_PATH)
     public Page<UserDTO> getAllUsers(@RequestParam(required = false) Integer pageNumber,
-                                      @RequestParam(required = false) Integer pageSize){
+                                     @RequestParam(required = false) Integer pageSize) {
         return userService.getAllUsers(pageNumber, pageSize);
+    }
+
+    @GetMapping(USER_PATH_ID)
+    public ResponseEntity<UserDTO> getUserById(@PathVariable("userId") UUID userId) {
+        return ResponseEntity.ok(userService.getUserById(userId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND)));
     }
 }
--- src/main/java/com/example/waytogo/user/service/api/UserService.java
+++ src/main/java/com/example/waytogo/user/service/api/UserService.java
@@ -3,6 +3,11 @@ package com.example.waytogo.user.service.api;
 import com.example.waytogo.user.model.dto.UserDTO;
 import org.springframework.data.domain.Page;
 
+import java.util.Optional;
+import java.util.UUID;
+
 public interface UserService {
     Page<UserDTO> getAllUsers(Integer pageNumber, Integer pageSize);
+
+    Optional<UserDTO> getUserById(UUID userId);
 }
