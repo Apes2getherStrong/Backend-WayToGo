38fec70088ddfb4067f52b3ac415ca18a5067563
Kondziow <konradwojtanowski02@gmail.com>
2023-11-23 12:32:02 +0100

    Troche poprawek roznego rodzaju

--- src/main/java/com/example/waytogo/user/model/entity/User.java
+++ src/main/java/com/example/waytogo/user/model/entity/User.java
@@ -3,6 +3,9 @@ package com.example.waytogo.user.model.entity;
 import com.example.waytogo.audio.model.entity.Audio;
 import com.example.waytogo.route.model.entity.Route;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import java.util.List;
@@ -22,15 +25,27 @@ public class User {
     @Column(name = "user_id", updatable = false, nullable = false)
     UUID userId;
 
+    @NotNull
+    @NotBlank
+    @Size(max = 20)
+    @Column(length = 20)
     String username;
 
+    @NotNull
+    @NotBlank
+    @Size(max = 20)
+    @Column(length = 20)
     String password;
 
+    @NotNull
+    @NotBlank
+    @Size(max = 50)
+    @Column(length = 50)
     String login;
 
-    @OneToMany(mappedBy = "user")
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE)
     List<Audio> audios;
 
-    @OneToMany(mappedBy = "user")
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE)
     List<Route> routes;
 }
