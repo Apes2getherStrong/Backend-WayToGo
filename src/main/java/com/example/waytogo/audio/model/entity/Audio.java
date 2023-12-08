bd21a38b875fbcbef085416f216b3933c001a7e8
Janek <62665388+Janek1010@users.noreply.github.com>
2023-12-08 21:09:33 +0100

    Revert "14 routes testsdata validation exceptions"

--- src/main/java/com/example/waytogo/audio/model/entity/Audio.java
+++ src/main/java/com/example/waytogo/audio/model/entity/Audio.java
@@ -4,16 +4,38 @@ package com.example.waytogo.audio.model.entity;
 import com.example.waytogo.point.model.entity.Point;
 import com.example.waytogo.user.model.entity.User;
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.UUID;
 
+@EqualsAndHashCode
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
+@Table(name = "audios")
 public class Audio {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "audio_id", updatable = false, nullable = false)
     UUID uuid;
+
+    @Column(name = "audio_name")
+    String name;
+
+/*
+    @Transient
+    MultipartFile file;
+
+    @Column(name = "file_path")
+    String filePath;
+*/
 
     @ManyToOne
     User user;
+
     @ManyToOne( fetch = FetchType.LAZY)
     @JoinColumn(name="point_id")
     Point point;
--- src/main/java/com/example/waytogo/user/service/impl/UserServiceJPA.java
+++ src/main/java/com/example/waytogo/user/service/impl/UserServiceJPA.java
@@ -48,7 +48,7 @@ public class UserServiceJPA implements UserService {
     @Override
     public UserDTO updateUserById(UUID userId, UserDTO userDTO) {
         userDTO.setUserId(userId);
-        return this.saveNewUser(userDTO);
+        return userMapper.userToUserDto(userRepository.save(userMapper.userDtoToUser(userDTO)));
     }
 
     @Override
@@ -93,7 +93,7 @@ public class UserServiceJPA implements UserService {
             }
         }
 
-        Sort sort = Sort.by(Sort.Order.asc("userName"));
+        Sort sort = Sort.by(Sort.Order.asc("username"));
 
         return PageRequest.of(queryPageNumber, queryPageSize, sort);
     }
