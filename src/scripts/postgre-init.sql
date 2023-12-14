d13b6205d9923161e981702dd41d9f62a55e1d22
Janek1010 <janekdomozych99@gmail.com>
2023-12-14 19:51:56 +0100

    FIX: postgre-init.sql is now removing user properly

--- src/scripts/postgre-init.sql
+++ src/scripts/postgre-init.sql
@@ -1,14 +1,12 @@
--- Tworzenie bazy danych
---CREATE DATABASE waytogo;
+--REASSIGN OWNED BY waytogo TO postgres;  -- or some other trusted role
+--DROP OWNED BY waytogo;
+DROP USER IF EXISTS waytogo;
 
--- Tworzenie użytkownika
-CREATE USER IF EXISTS waytogo WITH PASSWORD 'waytogo';
+CREATE USER  waytogo WITH PASSWORD 'waytogo';
 
--- Ustawienia użytkownika
 ALTER ROLE waytogo SET client_encoding TO 'utf8';
 ALTER ROLE waytogo SET default_transaction_isolation TO 'read committed';
 ALTER ROLE waytogo SET timezone TO 'UTC';
 
--- Nadanie wszystkich uprawnień użytkownikowi do nowej bazy danych
 GRANT ALL PRIVILEGES ON DATABASE waytogo TO waytogo;
 GRANT ALL PRIVILEGES ON SCHEMA public TO waytogo;
