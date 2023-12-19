1ee6d87468b34ad7831178d68a439aa8fb9795ed
Kondziow <konradwojtanowski02@gmail.com>
2023-12-19 11:58:29 +0100

    MapLocationRepositoryTest

--- src/test/java/com/example/waytogo/maplocation/repository/MapLocationRepositoryTest.java
+++ src/test/java/com/example/waytogo/maplocation/repository/MapLocationRepositoryTest.java
@@ -22,9 +22,10 @@ class MapLocationRepositoryTest {
 
     @Autowired
     GeometryFactory geometryFactory;
+
     @Test
     void testSaveMapLocation() {
-        MapLocation mapLocation =  mapLocationRepository.save(getMapLocation());
+        MapLocation mapLocation = mapLocationRepository.save(getMapLocation());
         mapLocationRepository.flush();
 
         assertThat(mapLocation).isNotNull();
@@ -35,17 +36,29 @@ class MapLocationRepositoryTest {
     @Test
     void testMapLocationNameTooLong() {
         assertThrows(Exception.class, () -> {
-            MapLocation mapLocation =  mapLocationRepository.save(MapLocation.builder()
+            MapLocation mapLocation = mapLocationRepository.save(MapLocation.builder()
                     .name("My MapLocation 01233456789012334567890123345678901233456789012334567890123345678901233456789012334567890123345678901233456789012334567890123345678901233456789012334567890123345678901233456789012334567890123345678901233456789012334567890123345678901233456789012334567890123345678901233456789012334567890123345678901233456789012334567890123345678901233456789012334567890123345678901233456789012334567890123345678901233456789012334567890123345678901233456789")
-                    .coordinates(geometryFactory.createPoint(new Coordinate(300,300)))
+                    .coordinates(geometryFactory.createPoint(new Coordinate(300, 300)))
                     .build());
             mapLocationRepository.flush();
         });
 
         assertThrows(Exception.class, () -> {
-            MapLocation mapLocation =  mapLocationRepository.save(MapLocation.builder()
+            MapLocation mapLocation = mapLocationRepository.save(MapLocation.builder()
                     .name("My MapLocation 456789")
-                    .coordinates(geometryFactory.createPoint(new Coordinate(300,300)))
+                    .coordinates(geometryFactory.createPoint(new Coordinate(300, 300)))
+                    .build());
+            mapLocationRepository.flush();
+        });
+    }
+
+    @Test
+    void testMapLocationDescriptionTooLong() {
+        assertThrows(Exception.class, () -> {
+            MapLocation mapLocation = mapLocationRepository.save(MapLocation.builder()
+                    .name("My MapLocation")
+                    .description("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+                    .coordinates(geometryFactory.createPoint(new Coordinate(300, 300)))
                     .build());
             mapLocationRepository.flush();
         });
@@ -54,7 +67,8 @@ class MapLocationRepositoryTest {
     MapLocation getMapLocation() {
         return MapLocation.builder()
                 .name("Test Name")
-                .coordinates(geometryFactory.createPoint(new Coordinate(22.2,31.2)))
+                .description("desc")
+                .coordinates(geometryFactory.createPoint(new Coordinate(22.2, 31.2)))
                 .build();
     }
 }
