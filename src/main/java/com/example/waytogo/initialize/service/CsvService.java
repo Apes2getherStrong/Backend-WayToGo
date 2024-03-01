159825629040520e400912ce3faa2aca1044b402
Janek1010 <janekdomozych99@gmail.com>
2024-03-01 15:37:15 +0100

    REFACTORING: reprojecting csv converter to having static generic method

--- src/main/java/com/example/waytogo/initialize/service/CsvService.java
+++
@@ -1,9 +0,0 @@
-package com.example.waytogo.initialize.service;
-
-
-import java.io.File;
-import java.util.List;
-
-public interface CsvService<T> {
-    List<T> convertCsvFileToCsvModel(File csvFile, Class<T> clazz);
-}
