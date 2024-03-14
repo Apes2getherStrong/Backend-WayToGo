0660e3277dd876a5f2efcf1f8864490ce8af93c3
Janek1010 <janekdomozych99@gmail.com>
2024-03-14 10:54:44 +0100

    FEATURE: compose sie laczyc ale jakis problem ze sciezkami

---
+++ db/init.sql
@@ -0,0 +1,13 @@
+CREATE EXTENSION IF NOT EXISTS postgis;
+-- DROP USER IF EXISTS waytogo;
+--
+-- CREATE USER waytogo WITH PASSWORD 'waytogo';
+--
+-- ALTER ROLE waytogo SET client_encoding TO 'utf8';
+-- ALTER ROLE waytogo SET default_transaction_isolation TO 'read committed';
+-- ALTER ROLE waytogo SET timezone TO 'UTC';
+--
+-- GRANT ALL PRIVILEGES ON DATABASE waytogo TO waytogo;
+-- GRANT ALL PRIVILEGES ON SCHEMA public TO waytogo;
+
+-- GRANT CREATE ON SCHEMA public TO waytogo;
