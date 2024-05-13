ec282123f6e97ee44851bbbf26568aa9ea3c62ee
Drillllll <118617644+Drillllll@users.noreply.github.com>
2024-05-13 16:56:45 +0200

    added putting / getting map location image

--- src/main/java/com/example/waytogo/maplocation/service/api/MapLocationService.java
+++ src/main/java/com/example/waytogo/maplocation/service/api/MapLocationService.java
@@ -4,6 +4,7 @@ import com.example.waytogo.maplocation.model.dto.MapLocationDTO;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.Optional;
@@ -22,4 +23,8 @@ public interface MapLocationService {
     Optional<MapLocationDTO> updateMapLocationById(UUID mapLocationId, @Valid MapLocationDTO mapLocationDTO);
 
     Optional<MapLocationDTO> patchMapLocationById(UUID mapLocationId, MapLocationDTO mapLocationDTO);
+
+    Boolean saveNewImage (MultipartFile file, UUID mapLocationId) throws IOException;
+
+    Optional<byte[]> getImageByMapLocationId(UUID mapLocationId) throws IOException;
 }
